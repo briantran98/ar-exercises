b2296dbc7bd714c66715d8dbf591a3e372cbbf4d
briantran98 <98brian.tran@gmail.com>
2020-07-20 23:50:41 +0000

    Added all validation for when creating a item in the database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name:'Khurram', last_name:'Virani', hourly_rate: 60)
+@store1.employees.create(first_name:'Glen', last_name:'Chen', hourly_rate: 55)
+@store1.employees.create(first_name:'Nima', last_name:'Nima', hourly_rate: 58)
+@store2.employees.create(first_name:'Jeremy', last_name:'Holman', hourly_rate: 65)
+@store2.employees.create(first_name:'Max', last_name:'Kevalenkov', hourly_rate: 58)
+@store2.employees.create(first_name:'Aaron', last_name:'Durfall', hourly_rate: 50)
+@store2.employees.create(first_name:"CC", last_name:"BB", hourly_rate: 50)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+	has_many :employees
 
+	validates :name, length: {minimum: 3}
+	validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validate :inventory_check, on: :create
+
+	def inventory_check
+		if !mens_apparel && !womens_apparel
+			errors.add("Mens and womens apparel can't be false")
+		end
+	end
 end
